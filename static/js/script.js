31b51feaa87dc4642383241ed8142e78d9059bea
yipmunallen <ymallen@hotmail.co.uk>
2021-03-23 22:27:15 +0000

    Add on click function for editing comments in "script.js"

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).on('click',function(){
-    $('.collapse').collapse('hide');
+    $('.navbar-collapse').collapse('hide');
 });
 
+$('#edit-comment').on('click',function(){
+
+    $('#comment-body').css('display','none');
+    $('#edit-comment-form').css('display','block');
+    $('#edit-comment').css('display','none');
+    $('#delete-comment').css('display','none');
+
+});
