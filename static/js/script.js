47925bbe261def1b6f8a53b918dc5cf8b446b9a0
yipmunallen <ymallen@hotmail.co.uk>
2021-04-04 10:36:34 +0000

    automatically close alerts in "script.js"

--- static/js/script.js
+++ static/js/script.js
@@ -19,3 +19,9 @@ $('#cancel-edit-comment').on('click',function(){
     $('#delete-comment').css('display','block');
 
 });
+
+// https://stackoverflow.com/questions/7643308/how-to-automatically-close-alerts-using-twitter-bootstrap
+$(document).ready(function(){
+    $(".comment-alerts").alert();
+    window.setTimeout(function() { $(".comment-alerts").alert('close'); }, 4000);
+});
