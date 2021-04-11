3897e9c90a4dd7d9846c857c4f4ed6ffe1f8c633
yipmunallen <ymallen@hotmail.co.uk>
2021-04-11 20:52:42 +0000

    edit comment function updated for unique ids in "script.js"

--- static/js/script.js
+++ static/js/script.js
@@ -3,25 +3,34 @@ $(document).ready(function(){
     $(".comment-alerts").alert();
     window.setTimeout(function() { $(".comment-alerts").alert('close'); }, 3000);
     $('.dropdown-toggle').dropdown();
+
+    $('.edit-comment').each(function(){
+    
+        var comment_body = 'comment-body-' + $(this).attr('id');
+        var comment_form = 'edit-comment-form-' + $(this).attr('id');
+        var delete_comment_button = 'delete-comment-' + $(this).attr('id');
+    
+        $(this).click(function(){
+            $('#'+comment_body).css('display','none');
+            $('#'+comment_form).css('display','block');
+            // Hide all other edit buttons ( you can only edit one at a time )
+            $('.edit-comment').css('display','none');
+            $('#'+delete_comment_button).css('display','none');
+        });
+    
+    });
+
+    $('.cancel-edit-comment').on('click',function(){
+
+        $('.comment-body').css('display','block');
+        $('.edit-comment-form').css('display','none');
+        $('.edit-comment').css('display','block');
+        $('.delete-comment').css('display','block');
+
+    });
 });
 
 $(document).on('click',function(){
     $('.navbar-collapse').collapse('hide');
 });
 
-$('#edit-comment').on('click',function(){
-
-    $('#comment-body').css('display','none');
-    $('#edit-comment-form').css('display','block');
-    $('#edit-comment').css('display','none');
-    $('#delete-comment').css('display','none');
-
-});
-
-$('#cancel-edit-comment').on('click',function(){
-
-    $('#comment-body').css('display','block');
-    $('#edit-comment-form').css('display','none');
-    $('#edit-comment').css('display','block');
-    $('#delete-comment').css('display','block');
-})
