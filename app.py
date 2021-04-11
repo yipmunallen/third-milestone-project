e574c2459fe199bcdd55693bc9d49456ffe9d064
yipmunallen <ymallen@hotmail.co.uk>
2021-04-11 14:29:53 +0000

    debug set to false in "app.py"

--- app.py
+++ app.py
@@ -385,5 +385,4 @@ def method_not_allowed(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            # Make sure to set debug to false when moved to PROD
-            debug=True)
+            debug=False)
